26d991fc1047fdcc6c34b79293b1fe9fd4d65288
Jenovai <andrea.jenovai@gmail.com>
2021-04-05 19:04:08 +0200

    animek adatok frissités

---
+++ js/adatlapok.js
@@ -0,0 +1,15 @@
+function horimiya(){
+    let animek = document.getElementById("animek")
+    animek.style.display = "none"
+
+    let hori = document.getElementById("horimiya")
+    hori.style.display = "block"
+}
+
+function shaman(){
+    let animek = document.getElementById("animek")
+    animek.style.display = "none"
+
+    let sh = document.getElementById("shaman_king")
+    sh.style.display = "block"
+}
