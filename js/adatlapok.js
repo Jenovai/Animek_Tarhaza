5bd5396cb2d44e139017f0eef627da5b99e6a354
Jenovai <andrea.jenovai@gmail.com>
2021-04-06 15:20:14 +0200

    dororo update

--- js/adatlapok.js
+++ js/adatlapok.js
@@ -13,3 +13,11 @@ function shaman(){
     let sh = document.getElementById("shaman_king")
     sh.style.display = "block"
 }
+
+function dororo(){
+    let animek = document.getElementById("animek")
+    animek.style.display = "none"
+
+    let dororo = document.getElementById("dororo")
+    dororo.style.display = "block"
+}
